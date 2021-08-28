739b495412bffc76f60b43a52608d603f75f6c06
Ke Wang <wangkekekexili@gmail.com>
2021-08-28 22:15:52 +0800

    feat: update cat img and enemy logic

--- main.ts
+++ main.ts
@@ -17,6 +17,24 @@ const imgPlaceholder = img`
     . . . . . . . . . . . . . . . .
 `
 
+const catImg = img`
+    . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e b f b . 
+    f d d f d d f d d f f d f . 
+    f b d d b b d d 2 b f d f . 
+    . f 2 2 2 2 2 2 d b d b f . 
+    . f d d d d d d d f f f . . 
+    . f d b d f f f d f . . . . 
+    . . f f f f . . f f . . . . 
+    `
+catImg.flipX()
+
 namespace SpriteKind {
     export const Coin = SpriteKind.create()
     export const Flower = SpriteKind.create()
@@ -35,11 +53,19 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Flower, function(cat, flower) {
     let bee = sprites.create(imgPlaceholder, SpriteKind.Enemy)
     animation.runImageAnimation(bee, assets.animation`beeAnimation`, 100, true)
     bee.setPosition(cat.x + 50, cat.y - 50)
+    if (bee.y < 0) {
+        bee.y = 0
+    }
     bee.follow(cat, 50, 200)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(cat, bee) {
+    if (Math.floor(cat.y) + cat.height/2 <= Math.floor(bee.y)) {
+        info.changeScoreBy(3)
+        cat.vy = -100
+    } else {
+        info.changeLifeBy(-1)
+    }
     bee.destroy()
-    info.changeLifeBy(-1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (cat.vy == 0) {
@@ -52,27 +78,20 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`portal`, function (sprite, lo
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lava`, function (sprite, location) {
     game.over(false, effects.melt)
 })
+let lastVx = 1
+game.onUpdate(function() {
+    const vx = cat.vx
+    if (vx > 0 && lastVx < 0 || vx < 0 && lastVx > 0) {
+        cat.image.flipX()
+        lastVx = vx
+    }
+})
 info.setLife(3)
 let coin: Sprite = null
 let flower: Sprite = null
 let cat: Sprite = null
 scene.setBackgroundColor(9)
-cat = sprites.create(img`
-    . . . . . . . . . . . . . . 
-    e e e . . . . e e e . . . . 
-    c d d c . . c d d c . . . . 
-    c b d d f f d d b c . . . . 
-    c 3 b d d b d b 3 c . . . . 
-    f b 3 d d d d 3 b f . . . . 
-    e d d d d d d d d e . . . . 
-    e d f d d d d f d e b f b . 
-    f d d f d d f d d f f d f . 
-    f b d d b b d d 2 b f d f . 
-    . f 2 2 2 2 2 2 d b d b f . 
-    . f d d d d d d d f f f . . 
-    . f d b d f f f d f . . . . 
-    . . f f f f . . f f . . . . 
-    `, SpriteKind.Player)
+cat = sprites.create(catImg, SpriteKind.Player)
 cat.ay = 350
 controller.moveSprite(cat, 100, 0)
 scene.cameraFollowSprite(cat)
