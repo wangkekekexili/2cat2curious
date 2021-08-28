7f94109d85424df181f5fd776bcee1cabcd3b2d5
Ke Wang <wangkekekexili@gmail.com>
2021-08-28 11:11:36 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`lava`, function (sprite, location) {
+    game.over(false, effects.melt);
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cat.vy == 0) {
+        cat.vy = -170
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (cat.vy == 0) {
+        cat.vy = -170
+    }
+})
+let cat: Sprite = null
+scene.setBackgroundColor(9)
+cat = sprites.create(img`
+    . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e b f b . 
+    f d d f d d f d d f f d f . 
+    f b d d b b d d 2 b f d f . 
+    . f 2 2 2 2 2 2 d b d b f . 
+    . f d d d d d d d f f f . . 
+    . f d b d f f f d f . . . . 
+    . . f f f f . . f f . . . . 
+    `, SpriteKind.Player)
+cat.ay = 350
+controller.moveSprite(cat, 100, 0)
+scene.cameraFollowSprite(cat)
+tiles.setTilemap(tilemap`level1`)
