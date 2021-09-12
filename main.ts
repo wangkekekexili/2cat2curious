34787651310b64ce232d81120f7c46ba6179c3da
Ke Wang <wangkekekexili@gmail.com>
2021-09-12 21:29:05 +0800

    Better cat images

--- main.ts
+++ main.ts
@@ -17,23 +17,73 @@ const imgPlaceholder = img`
     . . . . . . . . . . . . . . . .
 `
 
-const catImg = img`
-    . . . . . . . . . . . . . . 
-    e e e . . . . e e e . . . . 
-    c d d c . . c d d c . . . . 
-    c b d d f f d d b c . . . . 
-    c 3 b d d b d b 3 c . . . . 
-    f b 3 d d d d 3 b f . . . . 
-    e d d d d d d d d e . . . . 
-    e d f d d d d f d e b f b . 
-    f d d f d d f d d f f d f . 
-    f b d d b b d d 2 b f d f . 
-    . f 2 2 2 2 2 2 d b d b f . 
-    . f d d d d d d d f f f . . 
-    . f d b d f f f d f . . . . 
-    . . f f f f . . f f . . . . 
+namespace CatImg {
+    export const ToRight = img`
+    . . . . . . . . . . . . . .
+    . . . . e e e . . . . e e e
+    . . . . c d d c . . c d d c
+    . . . . c b d d f f d d b c
+    . . . . c 3 b d b d d b 3 c
+    . . . . f b 3 d d d d 3 b f
+    . . . . e d d d d d d d d e
+    . b f b e d f d d d d f d e
+    . f d f f d d f d d f d d f
+    . f d f b 2 d d b b d d b f
+    . f b d b d 2 2 2 2 2 2 f .
+    . . f f f d d d d d d d f .
+    . . . . f d f f f d b d f .
+    . . . . f f . . f f f f . .
     `
-catImg.flipX()
+    export const ToLeft = img`
+        . . . . . . . . . . . . . .
+        e e e . . . . e e e . . . .
+        c d d c . . c d d c . . . .
+        c b d d f f d d b c . . . .
+        c 3 b d d b d b 3 c . . . .
+        f b 3 d d d d 3 b f . . . .
+        e d d d d d d d d e . . . .
+        e d f d d d d f d e b f b .
+        f d d f d d f d d f f d f .
+        f b d d b b d d 2 b f d f .
+        . f 2 2 2 2 2 2 d b d b f .
+        . f d d d d d d d f f f . .
+        . f d b d f f f d f . . . .
+        . . f f f f . . f f . . . .
+    `
+    export const JumpToRight = img`
+        . . . . . . . . . . . . . .
+        . . . . e e e . . . . e e e
+        . . . . c d d c . . c d d c
+        . . . . c b d d f f d d b c
+        . . . . c 3 b d b d d b 3 c
+        . . . . f b 3 d d d d 3 b f
+        . . . . e d d d d d d d d e
+        . b f b e d f d d d d f d e
+        . f d f f d d f d d f d d f
+        . f d f b 2 d d b b d d b f
+        . f b d b d 2 2 2 2 2 2 f .
+        . . f f f f f f f f f f f .
+        . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . .
+    `
+    export const JumpToLeft = img`
+        . . . . . . . . . . . . . .
+        e e e . . . . e e e . . . .
+        c d d c . . c d d c . . . .
+        c b d d f f d d b c . . . .
+        c 3 b d d b d b 3 c . . . .
+        f b 3 d d d d 3 b f . . . .
+        e d d d d d d d d e . . . .
+        e d f d d d d f d e b f b .
+        f d d f d d f d d f f d f .
+        f b d d b b d d 2 b f d f .
+        . f 2 2 2 2 2 2 d b d b f .
+        . f f f f f f f f f f f . .
+        . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . .
+    `
+}
+
 
 namespace SpriteKind {
     export const Coin = SpriteKind.create()
@@ -81,9 +131,35 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`lava`, function (sprite, loca
 let lastVx = 1
 game.onUpdate(function() {
     const vx = cat.vx
-    if (vx > 0 && lastVx < 0 || vx < 0 && lastVx > 0) {
-        cat.image.flipX()
-        lastVx = vx
+    const vy = cat.vy
+    if (vx > 0) {
+        if (vy < 0) {
+            cat.setImage(CatImg.JumpToRight);
+        } else {
+            cat.setImage(CatImg.ToRight);
+        }
+        lastVx = vx;
+    } else if (vx < 0) {
+        if (vy < 0) {
+            cat.setImage(CatImg.JumpToLeft);
+        } else {
+            cat.setImage(CatImg.ToLeft);
+        }
+        lastVx = vx;
+    } else {
+        if (vy < 0) {
+            if (lastVx < 0) {
+                cat.setImage(CatImg.JumpToLeft);
+            } else {
+                cat.setImage(CatImg.JumpToRight);
+            }
+        } else {
+            if (lastVx < 0) {
+                cat.setImage(CatImg.ToLeft);
+            } else {
+                cat.setImage(CatImg.ToRight);
+            }
+        }
     }
 })
 info.setLife(3)
@@ -91,7 +167,7 @@ let coin: Sprite = null
 let flower: Sprite = null
 let cat: Sprite = null
 scene.setBackgroundColor(9)
-cat = sprites.create(catImg, SpriteKind.Player)
+cat = sprites.create(CatImg.ToRight, SpriteKind.Player)
 cat.ay = 350
 controller.moveSprite(cat, 100, 0)
 scene.cameraFollowSprite(cat)
